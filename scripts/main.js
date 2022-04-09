b34c97bd72092655dd1066ef712168019e3b9cc4
Alexander <oops.krit@yandex.ru>
2022-04-09 19:40:04 +0300

    исправлены недочёты по проектной работе

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
-// Открытие и закрытие поп-апа
 let profileEditButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button')
+let closePopupButton = document.querySelector('.popup__close-button');
 let popupProfile = document.querySelector('.popup');
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__item_type_user-name');
+let jobInput = document.querySelector('.form__item_type_user-description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');  
 
+// Открытие и закрытие поп-апа
 function openPopup() {
     popupProfile.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
@@ -13,21 +18,15 @@ function closePopup() {
     popupProfile.classList.remove('popup_opened');
 }
 
-profileEditButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup); 
-
 // Сохранение данных профиля
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__item_type_user-name');
-let jobInput = document.querySelector('.form__item_type_user-description');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');  
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-    closePopup()
+    closePopup();
 }
 
+//Слушатели
 formElement.addEventListener('submit', formSubmitHandler);
+profileEditButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup); 
